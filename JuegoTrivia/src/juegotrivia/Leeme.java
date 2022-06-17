72f94837e587cc9f582b87554793dd0658f4fd84
bboteo <raigeki_07@yahoo.es>
2022-06-16 21:12:31 -0600

    Todo hasta hoy

---
+++ JuegoTrivia/src/juegotrivia/Leeme.java
@@ -0,0 +1,11 @@
+package juegotrivia;
+
+public class Leeme {
+    /*
+    Tiempo invertido en el proyecto:
+    Miercoles (2h) - Creacion de boceto en Pincel
+    Jueves (3h) - Levantamiento de Frames en NetBeans
+    Jueves () - Levantamiento de la Base de Datos en Dia
+    
+    */
+}
